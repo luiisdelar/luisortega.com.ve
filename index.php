8f2505f03868d50742915e81db5ce93fa6659e11
Luis <luiseduardo.ortega@unet.edu.ve>
2018-05-29 23:29:24 -0400

    adding slide

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 	<title>Construcción</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="personal/styles.css">
-
+	<link rel="stylesheet" type="text/css" href="personal/css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="personal/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="personal/css/bootstrap-social.css">
+	
 </head>
 
 <body>
@@ -25,14 +27,60 @@
 	<div class="main container">
 		
 		<div class="portada">
-			<h1>_Luis Ortega</h1>
+			<h1>Titulo principal</h1>
 			<h5>Sitio web personal de presentación</h5>	
 		</div>
 
-		<button class="btn-google">Iniciar</button>
+		<div class="boxslide"> 
 
+			<div class="slides">
+				<div>
+					<img src="personal/imgs/one.jpg">
+				</div>
+				
+				<div>
+					<img src="personal/imgs/two.jpg">
+				</div>
+
+				<div>
+					<img src="personal/imgs/three.jpg">
+				</div>
+
+				<div>
+					<img src="personal/imgs/four.jpg">
+				</div>
+				
+			</div>
+
+		</div>
 
 	</div>
 	
+	<script src="personal/js/jquery.js"></script>
+	<script src="personal/js/jquery.slides.js"></script>
+
+	<script>
+		$(function(){
+  $(".slides").slidesjs({
+    play: {
+      active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "slide",
+        // [string] Can be either "slide" or "fade".
+      interval: 3000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: true,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
+  });
+});
+	</script>	
 </body>
 </html>
