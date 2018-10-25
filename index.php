7767389a5a014c53c5de500f14af8123fdfd8fcc
Luis <luiseduardo.ortega@unet.edu.ve>
2018-10-25 19:51:33 -0400

    Algunos cambios

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+	<!DOCTYPE html>
 <html>
 
 <head>
@@ -9,13 +9,13 @@
 	<link rel="stylesheet" type="text/css" href="personal/css/fonts.css">
 	<link rel="stylesheet" type="text/css" href="personal/css/styles.css">
 	<link rel="	stylesheet" type="text/css" href="personal/css/bootstrap-social.css">
-	
+	<script src="personal/js/jquery.js"></script>
 </head>
 
 <body>
 	<div class="nav navbar-fixed-top">
 		<div class="container">
-			<ul class="nav nav-pills">
+			<ul class="nav nav-pills list-inline">
 			  <li role="presentation"><a class="navtextt" href="https://www.facebook.com/luisortegadlr/" target="_blank"><span class="icon-facebook2"></span></a></li>
 			  <li role="presentation"><a class="navtextt" href="https://www.instagram.com/luiisdelar/" target="_blank"><span class="icon-instagram"></span></a></li>
 			  <li role="presentation"><a class="navtextt" href="https://www.twitter.com/luiiscfc/" target="_blank"><span class="icon-twitter"></span></a></li>
@@ -30,72 +30,38 @@
 	<div class="main container">
 		
 		<div class="portada">
-			<h1>Titulo principal</h1>
-			<h5>Sitio web personal de presentación</h5>	
+			<h1>Luis Eduardo Ortega De La Rosa</h1>
 		</div>
 
-		<div class="boxslide"> 
+		<div class="row">
+  			<div class="col-lg-6">
+  				<div class="article">
+					<h3 class="text-center">Diseño/Creación del sitio</h3>
+					<p>Sabiendo que en la actualidad hay muchas formas de crear una pagina
+					web ya sea con ayuda de un Framework, con algun CMS (<em>Content Management System</em>), 
+					ya sea <a href="https://es.wordpress.com/">Wordpress</a>, <a href="https://www.godaddy.com/es">GoDaddy</a>, <a href="https://es.wix.com/">Wix</a>, etc. Este sitio esta hecho desde cero,  sin alguna plantilla previa ni con ayuda de algun editor de diseño (ejemplo Dreamweaver), usé Sublime Text como editor de codigo fuente. El sitio esta hecho "a pie" solo con la ayuda del inspector de Firefox que es el navegador que mas uso justamente porque me gusta mas su inspector de elementos que el de Chrome (aunque tambien seria bueno probar el de Opera, Safari e Internet Explorer). Ademas se usó tambien Bootstrap aunque este lo use para maquillar las cosas mas que todo igualmente hay que decir que es un Framework (de diseño) al fin y al cabo. Al momento de escribir este articulo no he usado aun Javascript pero tengo en mente usarlo para algun efecto quizas, con la Ayuda de Jquery y alguna que otra libreria que me consiga por ahí en la via.  </p>
+				</div>
+  			</div>
 
-			<div class="slides">
-				<div>
-					<img src="personal/imgs/one.jpg">
-				</div>
-				
-				<div>
-					<img src="personal/imgs/two.jpg">
-					<h1>Que buena imagen xD</h1>
-				</div>
-
-				<div>
-					<img src="personal/imgs/three.jpg">
-				</div>
-
-				<div>
-					<img src="personal/imgs/four.jpg">
-				</div>
-				
-			</div>
+  			<div class="col-lg-6">
+  				<div class="article">
+  					<h3 class="text-center">Habilidades/Conocimientos</h3>
+  					<p>bLaslkda sakjdksladjs ksksask dskaksks adkd s afsalkf sflfns s f ksl fflkssl s flfklnsl slkj lasf skjf salk skljsa fjsa s jsk jslkfslk sk s	</p>
+  				</div>
+  			</div>
 
 		</div>
 
-		<div class="aside list-group">
-			<ul class="list-group">
-				<a class="list-group-item">Nada que abre el pasaporte express</a>
-				<a class="list-group-item">Maduro dio prorroga para bolivar soberano</a>
-				<a class="list-group-item">Estan frias y caras</a>
-				<a class="list-group-item">Esta jeva no me habla claro si quiere unos besos o que coño</a>
-			</ul>
-		</div>
+		
+
+		
 
 	</div>
 	
-	<footer>Luis Eduardo Ortega De La Rosa 2018</footer>
-	
-	<script src="personal/js/jquery.js"></script>
-	<script src="personal/js/jquery.slides.js"></script>
 
-	<script>
-		$(function(){
-  $(".slides").slidesjs({
-    play: {
-      active: true,
-        // [boolean] Generate the play and stop buttons.
-        // You cannot use your own buttons. Sorry.
-      effect: "slide",
-        // [string] Can be either "slide" or "fade".
-      interval: 3000,
-        // [number] Time spent on each slide in milliseconds.
-      auto: true,
-        // [boolean] Start playing the slideshow on load.
-      swap: true,
-        // [boolean] show/hide stop and play buttons
-      pauseOnHover: false,
-        // [boolean] pause a playing slideshow on hover
-      restartDelay: 2500
-        // [number] restart delay on inactive slideshow
-    }
-  });
-});
-	</script>	
+
+	
+	
+
 </body>
 </html>
