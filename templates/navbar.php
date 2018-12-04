c8d7adbd91127573b99010d8c4fe10be689b7d79
Luis <luiseduardo.ortega@unet.edu.ve>
2018-12-03 23:51:24 -0400

    awesome fonts

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,3 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a href="">...Soy el texto que va en el nav...</a>
+		<div class="facebook">
+		<i class="fab fa-facebook-square"></i>
+		</div>
+		<i class="github fab fa-github-alt"></i>
+		<i class="instagram fab fa-instagram"></i>
+		<i class="telegram fab fa-telegram-plane"></i>
+		<i class="twitter fab fa-twitter"></i>
+		<i class="mail fas fa-at"></i>
+		<i class="fab fa-css3-alt"></i>
+		<i class="fab fa-js"></i>
+		<i class="fab fa-laravel"></i>
+		<i class="fab fa-php"></i>
+		<i class="fab fa-html5"></i>
+		<i class="fas fa-database"></i>
+		
 </nav>
