e5f9104139c549cc5f5c2497b06ac949eece3945
Luis <luiseduardo.ortega@unet.edu.ve>
2019-08-15 16:53:42 -0400

    reestructurando js

--- calculadoraUNET/js/logic.js
+++ calculadoraUNET/js/logic.js
@@ -1,103 +1,146 @@
 function aceptar() {
+	
+	var select = parseInt(document.getElementById('seleccion').value);
+	var capa = document.getElementById("flujo");
+	var elemento = document.getElementById("botonaceptar");
+	var input, input2, label, label2, diva, divb, titulo, spana, spanb;
+	var x = elemento.parentNode;
+	var volver = document.createElement("button");
+	var calcular = document.createElement("button");
 
-		var select = parseInt(document.getElementById('seleccion').value);
-		var capa = document.getElementById("flujo");
-		var elemento = document.getElementById("botonaceptar");
-		var input, input2, label, label2, diva, divb, titulo, spana, spanb;
-		var x = elemento.parentNode;
-		var volver = document.createElement("button");
-		var calcular = document.createElement("button");
+	calcular.innerHTML = 'Calcular';
+	volver.innerHTML = 'Volver';
+	volver.className = 'form-control btn btn-primary';	
+	calcular.className = 'form-control btn btn-primary';
 
-		calcular.innerHTML = 'Calcular';
-		volver.innerHTML = 'Volver';
-		volver.className = 'form-control btn btn-primary';	
-		calcular.className = 'form-control btn btn-primary';
+	// x.removeChild(elemento);
+	// elemento = document.getElementById("seleccion");
+	// x = elemento.parentNode;
+	// x.removeChild(elemento);
+	// elemento = document.getElementById("label1");
+	// x = elemento.parentNode;
+	// x.removeChild(elemento);
 
-		x.removeChild(elemento);
-		elemento = document.getElementById("seleccion");
-		x = elemento.parentNode;
-		x.removeChild(elemento);
-		elemento = document.getElementById("label1");
-		x = elemento.parentNode;
-		x.removeChild(elemento);
+	$('#container-2').remove();
+	$('.row-1').empty();
 
-		for (var i = 0; i < select ; i++) {
-			
-			titulo = document.createElement('h3');
-			titulo.innerHTML = "Parcial "+(i+1);
-			capa.appendChild(titulo);
-			divx = document.createElement('div');
-			diva = document.createElement("div");
-			divy = document.createElement("div");
-			divb = document.createElement("div"); 
-			input = document.createElement("input");
-			label = document.createElement("label");
-			input2 = document.createElement("input");
-			label2 = document.createElement("label");
-			spana = document.createElement('span');
-			spanb = document.createElement('span');
-			
-			
-			input.setAttribute("id","nota"+(i+1));
-			input.className = "form-control";
-			input2.setAttribute("id","porc"+(i+1));
-			input2.className = "form-control";
-			divx.className = "form-group col-lg-6";
-			divy.className = "form-group col-lg-6";
-			diva.className = "input-group";
-			divb.className = "input-group";
+	for (var i = 0; i < select ; i++) {
 		
-			spana.className = "input-group-addon";
-			spanb.className = "input-group-addon";
-			spana.innerHTML = "Nota: 1-100";
-			spanb.innerHTML = "Porcentaje: %";
-			input.setAttribute('aria-describedby','sizing-addon1');
-			input.setAttribute('type','number');
-			input2.setAttribute('aria-describedby','sizing-addon1');
-			input2.setAttribute('type','number');
-			
-			diva.appendChild(spana);
-			diva.appendChild(input);
-			divx.appendChild(diva);
-			capa.appendChild(divx);
+		$('.row-1').append(
+			$('<h3>',{
+				'text': 'Parcial '+(i+1)
+			}),
+			$('<div>',{
+				'class': 'form-group col-lg-6'
+			}).append(
+				$('<div>',{
+					'class': 'input-group' 
+				}).append(
+					$('<span>',{
+					'class': 'input-group-text',
+					'text': 'Nota: 1-100',
+					'id': 'basic-addon3'
+					}),
+					$('<input>',{
+							'id': 'nota'+(i+1),
+							'class': 'form-control',
+							'aria-describedby': 'basic-addon3',
+							'type': 'number'
+					})
+				)
+			),
+			$('<div>',{
+				'class': 'form-group col-lg-6',
+			}).append(
+				$('<div>',{
+					'class': 'input-group' 
+				}).append(
+					$('<span>',{
+						'class': 'input-group-text',
+						'text': 'Porcentaje: %',
+						'id': 'porc'+(i+1),
+					}),
+					$('<input>',{
+							'id': 'nota'+(i+1),
+							'class': 'form-control',
+							'aria-describedby': 'basic-addon3',
+							'type': 'number'
+					})
+				)
+			)
+		);
 
-			divb.appendChild(spanb);
-			divb.appendChild(input2);
-			divy.appendChild(divb);
-			capa.appendChild(divy);
-			//capa.appendChild(divb);
+		// titulo = document.createElement('h3');
+		// titulo.innerHTML = "Parcial "+(i+1);
+		// capa.appendChild(titulo);
+		// divx = document.createElement('div');
+		// diva = document.createElement("div");
+		// divy = document.createElement("div");
+		// divb = document.createElement("div"); 
+		// input = document.createElement("input");
+		// label = document.createElement("label");
+		// input2 = document.createElement("input");
+		// label2 = document.createElement("label");
+		// spana = document.createElement('span');
+		// spanb = document.createElement('span');
 		
-		}
+		
+		// input.setAttribute("id","nota"+(i+1));
+		// input.className = "form-control";
+		// input2.setAttribute("id","porc"+(i+1));
+		// input2.className = "form-control";
+		// divx.className = "form-group col-lg-6";
+		// divy.className = "form-group col-lg-6";
+		// diva.className = "input-group";
+		// divb.className = "input-group";
+	
+		// spana.className = "input-group-addon";
+		// spanb.className = "input-group-addon";
+		// spana.innerHTML = "Nota: 1-100";
+		// spanb.innerHTML = "Porcentaje: %";
+		// input.setAttribute('aria-describedby','sizing-addon1');
+		// input.setAttribute('type','number');
+		// input2.setAttribute('aria-describedby','sizing-addon1');
+		// input2.setAttribute('type','number');
+		
+		// diva.appendChild(spana);
+		// diva.appendChild(input);
+		// divx.appendChild(diva);
+		// capa.appendChild(divx);
 
-			div = document.createElement('div');
-			div.className = 'form-group acomodar';
-			calcular.setAttribute('onclick','calculos('+select+')');
-			div.appendChild(calcular);
-			capa.appendChild(div);
-			
-			div = document.createElement('div');
-			div.className = 'form-group acomodar';
-			volver.setAttribute('onclick','document.location="index.html"');
+		// divb.appendChild(spanb);
+		// divb.appendChild(input2);
+		// divy.appendChild(divb);
+		// capa.appendChild(divy);
+		//capa.appendChild(divb);
+	
+	}
 
-			div.appendChild(volver);
-			capa.appendChild(div);
+		div = document.createElement('div');
+		div.className = 'form-group acomodar';
+		calcular.setAttribute('onclick','calculos('+select+')');
+		div.appendChild(calcular);
+		capa.appendChild(div);
+		
+		div = document.createElement('div');
+		div.className = 'form-group acomodar';
+		volver.setAttribute('onclick','document.location="index.html"');
+
+		div.appendChild(volver);
+		capa.appendChild(div);
 		
 }
 
 function calculos(parciales) {
-	
 	var contNotas = 0, porcentajes = 0, notaFinal = 0;
 
-	for (var i = 0; i < parciales; i++) {
-		
+	for (var i = 0; i < parciales; i++) {	
 		contNotas = parseInt(document.getElementById('nota'+(i+1)).value);
 		porcentajes += parseInt(document.getElementById('porc'+(i+1)).value);
-
 		if (contNotas > 100 || contNotas < 0) {
 			alert("Error en notas");
 			aceptar();
 		}
-	
 	}
 
 	if (porcentajes != 100) {
