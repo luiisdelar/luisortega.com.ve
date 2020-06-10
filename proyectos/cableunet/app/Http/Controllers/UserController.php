452f4fc4e9170a115ebc653ba2346646a3729ecc
Luis <luisortegadlr@gmail.com>
2020-06-09 20:11:37 -0400

    Editing some styles

--- proyectos/cableunet/app/Http/Controllers/UserController.php
+++ proyectos/cableunet/app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ use App\Channel;
 class UserController extends Controller
 {   
 
-    /*  public function __construct(){
+    public function __construct(){
         $this->middleware('auth');
     }
     /**
